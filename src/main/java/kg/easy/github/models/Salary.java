d002d0f01dae333a1513619f99fb82238b1220af
Tonyajan <devu6ka_kg@mail.ru>
2021-07-02 20:33:36 +0600

    created entities

---
+++ src/main/java/kg/easy/github/models/Salary.java
@@ -0,0 +1,23 @@
+package kg.easy.github.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "salaries")
+
+public class Salary {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private double salary;
+    private Date startDate;
+    private Date endDate;
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+}
